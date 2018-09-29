e4d8b224532d053df5ed92205632802a218974b6
Mathias Raacke <mathias@raacke.info>
2018-09-29 15:17:18 +0200

    feat(*): export services

--- projects/oocx-ng/src/public_api.ts
+++ projects/oocx-ng/src/public_api.ts
@@ -18,3 +18,8 @@ export { DurationPipe } from './lib/pipes/duration.pipe';
 export { CalendarComponent } from './lib/components/calendar/calendar.component';
 export { InputDateComponent } from './lib/components/input-date/input-date.component';
 export { InputDateDirective } from './lib/directives/input-date.directive';
+
+export { AutocompleteDateService } from './lib/services/autocomplete-date.service';
+export { DataSourceService, DataSourceSubscription } from './lib/services/datasource.service';
+export { LabelIdGeneratorService } from './lib/services/label-id-generator.service';
+export { DomHelperService } from './lib/services/dom-helper.service';
